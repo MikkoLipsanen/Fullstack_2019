ef552ea1cf7a4344430fefef299cdace71598ba3
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-01-18 10:42:01 +0200

    Tehtävä 1.2.

--- Viikko_1/sovellus1/src/index.js
+++ Viikko_1/sovellus1/src/index.js
@@ -11,7 +11,7 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       <p>
@@ -21,32 +21,35 @@ const Content = (props) => {
   )
 }
 
+const Content = () => {
+  return (
+    <div>
+      <Part part="Reactin perusteet" exercises="10" />
+      <Part part="Tiedonvälitys propseilla" exercises="7" />
+      <Part part="Komponenttien tila" exercises="14"/>
+    </div>
+  )
+}
+
 const Total = (props) => {
-    return (
-      <div>
-        <p>
-          yhteensä {props.total} tehtävää
-        </p>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <p>
+        yhteensä {props.total} tehtävää
+      </p>
+    </div>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const total = 31
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content />
+      <Total total={total} />
     </div>
     )
 
