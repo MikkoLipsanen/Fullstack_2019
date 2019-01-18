29c5f0f3c1ca5195b1c45c04b860ad32bc0fcc2a
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-01-18 12:00:39 +0200

    Tehtävä 1.3.

--- Viikko_1/sovellus1/src/index.js
+++ Viikko_1/sovellus1/src/index.js
@@ -1,59 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = () => {
-  return (
-    <div>
-      <Part part="Reactin perusteet" exercises="10" />
-      <Part part="Tiedonvälitys propseilla" exercises="7" />
-      <Part part="Komponenttien tila" exercises="14"/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        yhteensä {props.total} tehtävää
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const total = 31
+  const course = 'Half Stack -sovelluskehitys' 
+
+  const part1 = {
+    name: 'Reactin perusteet',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Tiedonvälitys propseilla',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'Komponenttien tila',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={total} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
-    )
-
-
+  )
 }
+
+const Header = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+  
+  const Part = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <p>{props.name} {props.exercises}</p>
+      </div>
+    )
+  }
+  
+  const Content = (props) => {
+    return (
+      <div>
+        <Part name={props.part1.name} exercises={props.part1.exercises} />
+        <Part name={props.part2.name} exercises={props.part2.exercises} />
+        <Part name={props.part3.name} exercises={props.part3.exercises} />
+      </div>
+    )
+  }
+  
+  const Total = (props) => {
+    return (
+      <div>
+        <p>
+          yhteensä {props.total} tehtävää
+        </p>
+      </div>
+    )
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'))
