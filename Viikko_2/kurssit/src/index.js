c8f767d8928929a8f638f7f8bc4923d36b5e5c46
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-01-25 01:19:29 +0200

    Tehtävä 2.1.

---
+++ Viikko_2/kurssit/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = props =>
+  <h1>{props.course}</h1>
+
+const Part = props => 
+  <p>{props.part.name} {props.part.exercises}</p>
+
+
+const Content = props => {
+  const items = props.parts.map(part =>
+    <Part key={part.id}
+          part = {part} 
+    />
+  )
+  return items
+}
+
+const Course = props => (
+  <div>
+    <Header course = {props.course.name} />
+    <Content parts = {props.course.parts} />
+  </div> 
+
+)
+
+const App = () => {
+
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  } 
+
+  return (
+    <div>
+      <Course course = {course} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
